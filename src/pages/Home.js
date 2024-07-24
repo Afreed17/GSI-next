4f709d83db91590418729806a86e745ccffe750e
farzeen007 <farzeenshareef989@gmail.com>
2024-07-24 07:19:18 -0700

    updated home page -farzeen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
 import React from "react";
 import Header from "../components/Header";
 import Discount from "../components/Discount";
-<<<<<<< Updated upstream
 import Category from "../components/Category";
-=======
 import Footer from "../components/Footer";
->>>>>>> Stashed changes
 
 const Home = () => {
   return (
